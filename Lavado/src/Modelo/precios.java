117e942c876c502b2de61f72129c67b217c1e0f2
Wlfran <wlfranhernandez11@hotmail.es>
2021-10-15 13:36:20 -0500

    clasprecios

--- Lavado/src/Modelo/precios.java
+++ Lavado/src/Modelo/precios.java
@@ -5,10 +5,83 @@
  */
 package Modelo;
 
-/**
- *
- * @author Wlfran
- */
+
 public class precios {
+    public int auto, camioneta, combo1, combo2, combo3, lbasico, lespecial, dbasica, davanzada;
+
+    public int getAuto() {
+        return auto;
+    }
+
+    public void setAuto(int auto) {
+        this.auto = auto;
+    }
+
+    public int getCamioneta() {
+        return camioneta;
+    }
+
+    public void setCamioneta(int camioneta) {
+        this.camioneta = camioneta;
+    }
+
+    public int getCombo1() {
+        return combo1;
+    }
+
+    public void setCombo1(int combo1) {
+        this.combo1 = combo1;
+    }
+
+    public int getCombo2() {
+        return combo2;
+    }
+
+    public void setCombo2(int combo2) {
+        this.combo2 = combo2;
+    }
+
+    public int getCombo3() {
+        return combo3;
+    }
+
+    public void setCombo3(int combo3) {
+        this.combo3 = combo3;
+    }
+
+    public int getLbasico() {
+        return lbasico;
+    }
+
+    public void setLbasico(int lbasico) {
+        this.lbasico = lbasico;
+    }
+
+    public int getLespecial() {
+        return lespecial;
+    }
+
+    public void setLespecial(int lespecial) {
+        this.lespecial = lespecial;
+    }
+
+    public int getDbasica() {
+        return dbasica;
+    }
+
+    public void setDbasica(int dbasica) {
+        this.dbasica = dbasica;
+    }
+
+    public int getDavanzada() {
+        return davanzada;
+    }
+
+    public void setDavanzada(int davanzada) {
+        this.davanzada = davanzada;
+    }
+    
+    
+    
     
 }
